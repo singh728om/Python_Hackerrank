6a6505f9809d984a13a31300f1d3b6b399fb1381
Shashibhushan Singh <pixom.ai@gmail.com>
2020-10-27 21:55:50 +0530

    instruction for user input---> clean code

--- symdiff.py
+++ symdiff.py
@@ -5,3 +5,18 @@ o=input()
 a=(set(m).intersection(set(o))).pop('')
 b=set(m).union(set(o))
 print(b.discard(a))
+
+
+#input format
+
+#user input as integer
+#user input
+#calculate differnece between two input
+"""
+2
+123 1234
+2
+234 345
+ print-->differnece b/w two input """
+
+
