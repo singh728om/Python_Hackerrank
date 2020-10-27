8c454ea76905175c940ace26ef775dec685e2e0c
Shashibhushan Singh <pixom.ai@gmail.com>
2020-10-27 21:53:43 +0530

    particular user input  format

--- symdiff.py
+++ symdiff.py
@@ -5,3 +5,11 @@ o=input()
 a=(set(m).intersection(set(o))).pop('')
 b=set(m).union(set(o))
 print(b.discard(a))
+
+
+#input format
+
+#user input as integer
+#user input
+#calculate differnece between two input
+
