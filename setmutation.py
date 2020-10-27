9d4c5e6973872f6175f55dcf676e98e6ed3e65a6
Shashibhushan Singh <pixom.ai@gmail.com>
2020-10-27 22:07:00 +0530

    Update setmutation.py

--- setmutation.py
+++ setmutation.py
@@ -1,21 +1,16 @@
-n = int(input())
-count=0
-s = set(map(int, input().split()))
-m=int(input())
-for i in range (0,m):
-    l=input().split()
-    if (l[0]=="intersection_update"):
-        h=set(map(int, input().split()))
-        s.intersection_update(h)
-    elif (l[0]=="update"):
-        k=set(map(int, input().split()))
-        s.update(k)
-    elif (l[0]=="symmetric_difference_update"):
-        p=set(map(int, input().split()))
-        s.symmetric_difference_update(p)
-    elif (l[0]=="difference_update"):
-        q=set(map(int, input().split()))
-        s.difference_update(q)
-for j in range (0,len(s)):
-    count=count+int(list(s)[j])
-print(count)
+length=int(input())
+s=set(map(int,input().split()))
+N=int(input())
+
+for i in range(N):
+    (p, q)=input().split()
+    s2=set(map(int,input().split()))
+    if p=='intersection_update':
+        s.intersection_update(s2)
+    elif p=='update':
+        s.update(s2)
+    elif p=='symmetric_difference_update':
+        s.symmetric_difference_update(s2)
+    elif p=='difference_update':
+        s.difference_update(s2)
+print(sum(s))
